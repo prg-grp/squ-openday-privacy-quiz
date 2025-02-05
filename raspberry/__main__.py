238abfce2fc5d1d843195becf324a5943ce75b29
Christoph Bühler <christoph.buehler@unisg.ch>
2025-02-05 17:50:18 +0100

    use different keyboard package in python

--- raspberry/__main__.py
+++ raspberry/__main__.py
@@ -1,12 +1,28 @@
+import platform
 import signal
 
-import pyautogui
+import keyboard
 from gpiozero import Button
 
 button_pin_1 = 7
 button_pin_2 = 11
 button_pin_3 = 13
 button_pin_4 = 15
+
+
+def press(key):
+    keys = {
+        "a": 0,
+        "b": 11,
+        "c": 8,
+        "d": 2,
+        "r": 15,
+    }
+
+    if platform.system() == "Darwin":
+        keyboard.press_and_release(keys[key])
+    else:
+        keyboard.press_and_release(key)
 
 
 def main():
@@ -18,10 +34,10 @@ def main():
     button_3 = Button(button_pin_3)
     button_4 = Button(button_pin_4)
 
-    button_1.when_activated = lambda: pyautogui.press("1")
-    button_2.when_activated = lambda: pyautogui.press("2")
-    button_3.when_activated = lambda: pyautogui.press("3")
-    button_4.when_activated = lambda: pyautogui.press("4")
+    button_1.when_activated = lambda: keyboard.press_and_release("a")
+    button_2.when_activated = lambda: keyboard.press_and_release("b")
+    button_3.when_activated = lambda: keyboard.press_and_release("c")
+    button_4.when_activated = lambda: keyboard.press_and_release("d")
 
     print("To exit press Ctrl+C")
     signal.signal(signal.SIGINT, signal.SIG_DFL)
@@ -30,3 +46,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+    # import time
+
+    # time.sleep(5)
+    # print("Pressing a")
+    # press("a")
+    # time.sleep(5)
+    # print("Pressing b")
+    # press("b")
+    # time.sleep(5)
+    # print("Pressing c")
+    # press("c")
+    # time.sleep(5)
+    # print("Pressing d")
+    # press("d")
+    # time.sleep(5)
+    # print("Pressing r")
+    # press("r")
